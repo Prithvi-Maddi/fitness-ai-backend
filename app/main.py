3dd515a5920e9b5a58124e46071054a78685eeb1
Prithvi Maddi <prithvimaddi@Sureshs-MacBook-Pro-2.local>
2025-07-27 12:23:48 -0700

    add JWT generation utility

--- app/main.py
+++ app/main.py
@@ -16,6 +16,17 @@ SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 ALGORITHM = "HS256"
 
+def create_access_token(data:dict) -> str:
+    """ 
+    Create JWT token containing 'data' as the payload
+    Token will expire after ACCESS_TOKEN_EXPIRE_MINUTES
+    """
+    to_encode = data.copy()
+    expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
 class UserCreate(BaseModel):
     name: str
     email: EmailStr
