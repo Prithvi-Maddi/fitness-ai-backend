c86f36b2339fdaab333ba726bf4f15bb2b9fbd08
Prithvi Maddi <prithvimaddi@Sureshs-MacBook-Pro-2.local>
2025-08-02 23:08:25 -0700

    add dependency for JWT_protected routes

--- app/main.py
+++ app/main.py
@@ -119,3 +119,24 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
 
     #return token and token type
     return {"access_token": access_token, "token_type": "bearer"}
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+    return user
